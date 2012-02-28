fbf565ccb47ad6152c10031b9aa5620af1e802ac
Dan Smith <mrdanpsmith@gmail.com>
2012-02-27 23:38:56 -0500

    Removed mustache requirement, looks like the middle is going to be mostly JSON.

--- Gemfile
+++ Gemfile
@@ -2,5 +2,4 @@ source :rubygems
 gem 'sinatra'
 gem 'mongo_mapper'
 gem 'bson_ext'
-gem 'mustache'
 gem 'i18n'
--- chaoschat.rb
+++ chaoschat.rb
@@ -1,7 +1,6 @@
 require 'bundler/setup'
 require 'sinatra'
 require 'mongo_mapper'
-require 'mustache/sinatra'
 
 class ChaosChat < Sinatra::Base
 	get '/' do
