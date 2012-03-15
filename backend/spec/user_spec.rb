70bb00a199f0d13e8e47de4b350bc3b2ad470aea
Dan P. Smith <mrdanpsmith@gmail.com>
2012-03-14 20:04:31 -0400

    Starting specing some things out.

--- backend/spec/user_spec.rb
+++ backend/spec/user_spec.rb
@@ -3,5 +3,7 @@ require File.join(File.dirname(__FILE__), '..', 'user')
 require 'mongoid-rspec'
 
 describe User do
-	it { should have_field(:handle) }
+	it { should have_field(:handle).of_type(String) }
+	it { should validate_presence_of(:handle) }
+	it { should validate_uniqueness_of(:handle) }
 end
--- backend/user.rb
+++ backend/user.rb
@@ -2,5 +2,7 @@ require 'mongoid'
 
 class User
 	include Mongoid::Document
-	field :handle
+	field :handle, type: String
+	validates_presence_of :handle
+	validates_uniqueness_of :handle
 end
