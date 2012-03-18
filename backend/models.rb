13e83a48bd7ae5f92f43a297005a93de6d691063
Dan Smith <mrdanpsmith@gmail.com>
2012-03-18 15:03:54 -0400

    Got somewhat of an initial datamodel for a simple chat.

--- backend/models.rb
+++ backend/models.rb
@@ -1,7 +1,16 @@
 require 'mongoid'
 
+class User
+	include Mongoid::Document
+	field :name, type: String
+	validates_presence_of :name
+	validates_uniqueness_of :name
+	has_and_belongs_to_many :chats	
+end
+
 class Chat
 	include Mongoid::Document
+	has_and_belongs_to_many :users
 	embeds_many :posts
 end
 
--- backend/spec/models_spec.rb
+++ backend/spec/models_spec.rb
@@ -2,7 +2,15 @@ require 'spec_helper'
 require File.join(File.dirname(__FILE__), '..', 'models')
 require 'mongoid-rspec'
 
+describe User do
+	it { should have_field(:name).of_type(String) }
+	it { should validate_presence_of(:name) }
+	it { should validate_uniqueness_of(:name) }
+	it { should have_and_belong_to_many(:chats) }
+end
+
 describe Chat do
+	it { should have_and_belong_to_many(:users) }
 	it { should embed_many(:posts) }
 end
 
